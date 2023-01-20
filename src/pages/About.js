68b3b3b10c1b00e0e6cd0f548b0f9be9c4771dca
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-01-20 21:40:17 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero id enim laborum ipsa quasi quo animi, molestiae natus labore aspernatur deleniti illum nisi accusantium est iste, corrupti non sit quae aliquam ab voluptatibus tenetur. Temporibus blanditiis illo repellat dolorum voluptatibus?</p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <section className='error-page section'>
+       <div className="error-container">
+          <h2>oops! its a dead end</h2>
+          <Link to={'/'} className='btn'>back home</Link>
+       </div>
+    </section>
+  )
+}
+
+export default Error
